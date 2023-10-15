e877845a40ab649e11e70181c38dc27a0967ed2f
Andrei-David Nan <nandavid190@gmail.com>
2023-10-15 09:42:11 +0300

    v0.0.6 - Fixed possible casting issues in draw_text;

--- TextDrawer.cpp
+++ TextDrawer.cpp
@@ -1,40 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include "Headers/TextDrawer.hpp"
 
-void draw_text(bool horizontalCenter, bool vertical_center, short x, short y, const std::string& text, const sf::Color& color, sf::RenderWindow& window, const sf::Texture& font_texture) {
-    short char_x = x;
-    short char_y = y;
+void draw_text(bool horizontal_center, bool vertical_center, short x, short y, const std::string& text, const sf::Color& color, sf::RenderWindow& window, const sf::Texture& font_texture) {
+    auto char_x = static_cast<float>(x);
+    auto char_y = static_cast<float>(y);
 
-    unsigned char char_height = font_texture.getSize().y;
-    unsigned char char_width = font_texture.getSize().x / 96;
+    unsigned char char_height = static_cast<unsigned char>(font_texture.getSize().y);
+    unsigned char char_width = static_cast<unsigned char>(font_texture.getSize().x / 96);
 
     sf::Sprite char_sprite(font_texture);
     char_sprite.setColor(color);
 
-    if (horizontalCenter) {
-        char_x -= static_cast<short>(0.5f * char_width * text.substr(0, text.find_first_of('\n')).size());
+    if (horizontal_center) {
+        char_x -= 0.5f * char_width * static_cast<float>(text.substr(0, text.find_first_of('\n')).size()); // Cast to float
     }
 
     if (vertical_center) {
-        char_y -= static_cast<short>(0.5f * char_height * (1 + std::count(text.begin(), text.end(), '\n')));
+        char_y -= 0.5f * char_height * (1 + static_cast<float>(std::count(text.begin(), text.end(), '\n'))); // Cast to float
     }
 
     for (std::string::const_iterator it = text.begin(); it != text.end(); it++) {
         if (*it == '\n') {
-            if (horizontalCenter) {
-                char_x = x - static_cast<short>(0.5f * char_width * text.substr(0, text.find_first_of('\n')).size());
+            if (horizontal_center) {
+                char_x = static_cast<float>(x) - 0.5f * char_width * static_cast<float>(text.substr(0, text.find_first_of('\n')).size()); // Cast to float
             } else {
-                char_x = x;
+                char_x = static_cast<float>(x); // Cast to float
             }
 
-            char_y += char_height;
+            char_y += static_cast<float>(char_height); // Cast to float
             continue;
         }
 
         char_sprite.setPosition(char_x, char_y);
         char_sprite.setTextureRect(sf::IntRect(char_width * (*it - 32), 0, char_width, char_height));
 
-        char_x += char_width;
+        char_x += static_cast<float>(char_width);
 
         window.draw(char_sprite);
     }
